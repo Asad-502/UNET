d16c21d5f4d8ead9782f2fbfecc04414b490ae5e
Asad-502 <129690784+Asad-502@users.noreply.github.com>
2025-05-14 23:26:09 +0500

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -7,19 +7,19 @@ mkdir datasets
 # Download the datasets
 cd datasets
 kaggle datasets download -d andrewmvd/drive-digital-retinal-images-for-vessel-extraction
-wget -O stare.tar "https://www.dropbox.com/scl/fi/mfodfr67sqag047jkhntp/stare-DatasetNinja.tar?rlkey=z8kwhb5us58phsydquokt8kq1&dl=1"
+wget -O stare.zip https://cecas.clemson.edu/~ahoover/stare/images/all-images.zip
 wget -O hrf.zip https://www5.cs.fau.de/fileadmin/research/datasets/fundus-images/all.zip
-wget https://researchdata.kingston.ac.uk/96/1/CHASEDB1.zip
+wget -O Chase.zip https://researchdata.kingston.ac.uk/96/1/CHASEDB1.zip
 
 # Extract the datasets
-unzip drive-digital-retinal-images-for-vessel-extraction.zip
-tar -xvf stare.tar
-mv ds stare
-unzip hrf.zip -d hrf
-unzip CHASEDB1.zip -d chase
+unzip -o drive-digital-retinal-images-for-vessel-extraction.zip -d drive-digital-retinal-images
+unzip -o stare.zip -d stare
+
+unzip -o hrf.zip -d hrf
+unzip -o Chase.zip -d Chase
 
 # Remove the archive files
 rm drive-digital-retinal-images-for-vessel-extraction.zip
-rm stare.tar
+rm stare.zip
 rm hrf.zip
-rm CHASEDB1.zip
+rm Chase.zip
